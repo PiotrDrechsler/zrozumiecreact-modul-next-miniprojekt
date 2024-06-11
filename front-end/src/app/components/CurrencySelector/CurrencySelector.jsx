e97b40474147534b70ee09503cfa9c358e196dee
PiotrDrechsler <pdrech@gmail.com>
2024-06-11 19:37:56 +0200

    fix/apply currency context into app
    ~create useState hook in Layout component
    ~set PLN to default value in useState hook
    ~add values in currency context provider component
    ~add useContext handler to CurrencySelector component to choose currency
    ~apply currency context in Product.jsx component

--- front-end/src/app/components/CurrencySelector/CurrencySelector.jsx
+++ front-end/src/app/components/CurrencySelector/CurrencySelector.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+
 import styles from "./CurrencySelector.module.css";
 import { CURRENCIES } from "../../constants/currencies";
+import { CurrencyContext } from "@/app/contexts/CurrencyContext";
 
 export function CurrencySelector() {
-    return (
-        <select className={styles.currencySelector}>
-            <option value={CURRENCIES.PLN}>{CURRENCIES.PLN}</option>
-            <option value={CURRENCIES.USD}>{CURRENCIES.USD}</option>
-        </select>
-    );
+  const [selectedCurency, setSelectedCurrency] = useContext(CurrencyContext);
+  return (
+    <select
+      className={styles.currencySelector}
+      value={selectedCurency}
+      onChange={(e) => setSelectedCurrency(e.target.value)}
+    >
+      <option value={CURRENCIES.PLN}>{CURRENCIES.PLN}</option>
+      <option value={CURRENCIES.USD}>{CURRENCIES.USD}</option>
+    </select>
+  );
 }
--- front-end/src/app/components/Layout/Layout.jsx
+++ front-end/src/app/components/Layout/Layout.jsx
@@ -1,4 +1,6 @@
 "use client";
+import { useState } from "react";
+
 import { TopBar } from "../TopBar/TopBar";
 import { MainMenu } from "../MainMenu/MainMenu";
 import { Logo } from "../Logo/Logo";
@@ -8,11 +10,13 @@ import { Footer } from "../Footer/Footer";
 import { MainContent } from "../MainContent/MainContent";
 import { CurrencySelector } from "../CurrencySelector/CurrencySelector";
 import { CurrencyContext } from "@/app/contexts/CurrencyContext";
+import { CURRENCIES } from "@/app/constants/currencies";
 
 export function Layout({ children }) {
+  const [selectedCurency, setSelectedCurrency] = useState(CURRENCIES.PLN);
   return (
     <>
-      <CurrencyContext.Provider>
+      <CurrencyContext.Provider value={[selectedCurency, setSelectedCurrency]}>
         <MainContent>
           <TopBar>
             <MainMenu />
