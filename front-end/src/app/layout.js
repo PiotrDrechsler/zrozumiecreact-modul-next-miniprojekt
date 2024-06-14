2cb715de04861fa657e7a4b4d0fc29415baeeebb
PiotrDrechsler <pdrech@gmail.com>
2024-06-14 21:48:19 +0200

    fix/font
    apply main font to site

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,15 +1,22 @@
 import { Layout } from "./components/Layout/Layout";
 import globals from "./globals.css";
+import { Roboto } from "next/font/google";
 
 export const metadata = {
   title: "TOPSHOP",
   description: "Sklep e-commerce w Next.js",
 };
 
+const roboto = Roboto({
+  style: ["normal"],
+  subsets: ["latin"],
+  weight: "300",
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Layout>{children}</Layout>
       </body>
     </html>
